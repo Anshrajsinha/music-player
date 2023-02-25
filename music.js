b11d8d8e36e9f23e9524f72351aadff709120792
Anshrajsinha <123859760+Anshrajsinha@users.noreply.github.com>
2023-02-26 00:35:38 +0530

    Update music.js

--- music.js
+++ music.js
@@ -185,7 +185,7 @@ setInterval(() => {
 const initializePlaylist = () => {
     for(let i in songsList) {
         playlistSongs.innerHTML += 
-        `<li class="playlistSong" onclick="setSong(${i});audio.play();">
+        `<li class="playlistSong" onclick="setSong(${i});playAudio();">
             <div class="playlist-image-container">
               <img src="${songsList[i].image}"/>
             </div>
